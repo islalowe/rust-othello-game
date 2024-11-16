0eb22697459ba140110e49beefc76ec78d4d9c64
islalowe <lowek3@spu.edu>
2024-11-16 14:19:34 -0800

    test

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,9 @@ fn main() {
 // 8 x 8 board
 // 64 tokens
 // set up by placing:
-    // white tokens on row4, column4 and row5, column 5 and black tokens on row4, column5 and row5, column4
+    // white tokens on row4, column4 and row5, column5 and black tokens on row4, column5 and row5, column4
     // or
-    // black tokens on row4, column4 and row5, column 5 and white tokens on row4, column5 and row5, column4
+    // black tokens on row4, column4 and row5, column5 and white tokens on row4, column5 and row5, column4
 // each player is assigned a color 
 // random coin flip to decide who goes first, and then the players take turns
 // to make a move, flank an opposing color
@@ -29,5 +29,9 @@ fn main() {
 // include error handling and an welcome message that offers to explain gameplay rules
 
 /*  to make:
-// function to explain the rules (only called if user asks for it)
+> function to explain the rules (only called if user asks for it)
+> function to print the current state of the board
 */
+
+
+//testing github desktop
